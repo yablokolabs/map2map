f516b7be09d01013a215010238cf98b2d4c95333
CEO <ceo@map2map.com>
2025-06-22 02:31:09 +0530

    add apollo analytics

--- assets/js/include-footer.js
+++ assets/js/include-footer.js
@@ -35,6 +35,19 @@ function loadAnalytics() {
     ahrefsScript.dataset.key = 'OKf0ZdUa/hzhaw1hBscJLw';
     ahrefsScript.async = true;
     document.head.appendChild(ahrefsScript);
+    
+    // Load Apollo Analytics
+    const apolloScript = document.createElement('script');
+    apolloScript.text = `
+        function initApollo(){
+            var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+            o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+            o.onload=function(){window.trackingFunctions.onLoad({appId:"6855cd9864a89f0015143a0c"})},
+            document.head.appendChild(o)
+        }
+        initApollo();
+    `;
+    document.head.appendChild(apolloScript);
 }
 
 // Call the functions when the DOM is loaded
